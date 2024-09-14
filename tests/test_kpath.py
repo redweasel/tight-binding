a9f5ab4e6dbfbd1ee811ba0bc09d8db32dc78b97
redweasel <54547948+redweasel@users.noreply.github.com>
2024-09-14 21:29:51 +0200

    fix kpaths test by switch back to svg with postprocessing

--- tests/test_kpath.py
+++ tests/test_kpath.py
@@ -1,4 +1,5 @@
 import io
+import re
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.testing as mpl_test
@@ -7,61 +8,44 @@ mpl_test.setup()
 from tight_binding_redweasel import kpaths
 from tight_binding_redweasel import symmetry
 
-# disabled this test, as it doesn't work over multiple matplotlib versions...
-def _test_kpaths_plot_svg():
+def test_kpaths_plot_svg():
     # testing matplotlib results is a pain...
-    # the svg.hashsalt doesn't seem consistent between versions.
-    # so I have to either test png or figure out a way to remove the hashes from the output.
+    # svg doesn't work, because svg.hashsalt handling differs over multiple matplotlib versions.
+    # png doesn't work because it includes the matplotlib version, even with 'Creator': None
+    # pickle doesn't work because it includes the matplotlib version
+
+    # the solution is to use svg, but remove some id data using regex
+    read = True
 
     plt.figure(figsize=(5, 5), frameon=False)
     kpaths.SC_PATH.plot(lambda x: x, band_offset=1, label_bands="left", ylim=(-1.0, 1.0))
     s = io.StringIO()
     plt.savefig(s, format="svg", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
     svg = s.getvalue()
-    with open("tests/ref_kpath_left.svg", "r") as file:
-        f = file.read()
-        assert svg == f, f"generated svg ({len(svg)}) doesn't match saved svg ({len(f)})"
-    #with open("tests/ref_kpath_left.svg", "w") as file:
-    #    file.write(svg)
+    svg = re.sub(r"url\(#\w+\)", r"url\(\)", svg)
+    svg = re.sub(r'id="\w+"', r"", svg)
+    if read:
+        with open("tests/ref_kpath_left.svg", "r") as file:
+            f = file.read()
+            assert svg == f, f"generated svg ({len(svg)}) doesn't match saved svg ({len(f)})"
+    else:
+        with open("tests/ref_kpath_left.svg", "w") as file:
+            file.write(svg)
 
     plt.figure(figsize=(5, 5), frameon=False)
     kpaths.FCC_PATH.plot(lambda x: x, band_offset=1, label_bands="right", ylim=(-1.0, 1.0))
     s = io.StringIO()
     plt.savefig(s, format="svg", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
     svg = s.getvalue()
-    with open("tests/ref_kpath_right.svg", "r") as file:
-        f = file.read()
-        assert svg == f, f"generated svg ({len(svg)}) doesn't match saved svg ({len(f)})"
-    #with open("tests/ref_kpath_right.svg", "w") as file:
-    #    file.write(svg)
-
-
-def test_kpaths_plot_png():
-    # testing matplotlib results is a pain...
-    # the svg.hashsalt doesn't seem consistent between versions.
-    # so I have to either test png or figure out a way to remove the hashes from the output.
-
-    plt.figure(figsize=(5, 5), frameon=False)
-    kpaths.SC_PATH.plot(lambda x: x, band_offset=1, label_bands="left", ylim=(-1.0, 1.0))
-    s = io.BytesIO()
-    plt.savefig(s, format="png", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
-    png = s.getbuffer().tobytes()
-    with open("tests/ref_kpath_left.png", "rb") as file:
-        f = file.read()
-        assert png == f
-    #with open("tests/ref_kpath_left.png", "wb") as file:
-    #    file.write(png)
-
-    plt.figure(figsize=(5, 5), frameon=False)
-    kpaths.FCC_PATH.plot(lambda x: x, band_offset=1, label_bands="right", ylim=(-1.0, 1.0))
-    s = io.BytesIO()
-    plt.savefig(s, format="png", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
-    png = s.getbuffer().tobytes()
-    with open("tests/ref_kpath_right.png", "rb") as file:
-        f = file.read()
-        assert png == f
-    #with open("tests/ref_kpath_right.png", "wb") as file:
-    #    file.write(png)
+    svg = re.sub(r"url\(#\w+\)", r"url\(\)", svg)
+    svg = re.sub(r'id="\w+"', r"", svg)
+    if read:
+        with open("tests/ref_kpath_right.svg", "r") as file:
+            f = file.read()
+            assert svg == f, f"generated svg ({len(svg)}) doesn't match saved svg ({len(f)})"
+    else:
+        with open("tests/ref_kpath_right.svg", "w") as file:
+            file.write(svg)
 
 
 def test_kpaths_interpolate():
@@ -72,3 +56,5 @@ def test_kpaths_interpolate():
     for k, band in zip(k_smpl, bands):
         assert np.linalg.norm(interp(k) - band) < 1e-12, f"{interp(k)} != {band}"
         assert np.linalg.norm(interp(-k) - band) < 1e-12, f"{interp(-k)} != {band}"
+
+test_kpaths_plot_svg()
