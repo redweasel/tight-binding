c1e102aded5665bda32c38e135caa12da8cf640c
redweasel <54547948+redweasel@users.noreply.github.com>
2024-09-14 21:07:19 +0200

    change kpaths test to png for reproducibility

--- tests/test_kpath.py
+++ tests/test_kpath.py
@@ -7,7 +7,12 @@ mpl_test.setup()
 from tight_binding_redweasel import kpaths
 from tight_binding_redweasel import symmetry
 
-def test_kpaths_plot():
+# disabled this test, as it doesn't work over multiple matplotlib versions...
+def _test_kpaths_plot_svg():
+    # testing matplotlib results is a pain...
+    # the svg.hashsalt doesn't seem consistent between versions.
+    # so I have to either test png or figure out a way to remove the hashes from the output.
+
     plt.figure(figsize=(5, 5), frameon=False)
     kpaths.SC_PATH.plot(lambda x: x, band_offset=1, label_bands="left", ylim=(-1.0, 1.0))
     s = io.StringIO()
@@ -29,6 +34,34 @@ def test_kpaths_plot():
         assert svg == f, f"generated svg ({len(svg)}) doesn't match saved svg ({len(f)})"
     #with open("tests/ref_kpath_right.svg", "w") as file:
     #    file.write(svg)
+
+
+def test_kpaths_plot_png():
+    # testing matplotlib results is a pain...
+    # the svg.hashsalt doesn't seem consistent between versions.
+    # so I have to either test png or figure out a way to remove the hashes from the output.
+
+    plt.figure(figsize=(5, 5), frameon=False)
+    kpaths.SC_PATH.plot(lambda x: x, band_offset=1, label_bands="left", ylim=(-1.0, 1.0))
+    s = io.BytesIO()
+    plt.savefig(s, format="png", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
+    png = s.getbuffer().tobytes()
+    with open("tests/ref_kpath_left.png", "rb") as file:
+        f = file.read()
+        assert png == f
+    #with open("tests/ref_kpath_left.png", "wb") as file:
+    #    file.write(png)
+
+    plt.figure(figsize=(5, 5), frameon=False)
+    kpaths.FCC_PATH.plot(lambda x: x, band_offset=1, label_bands="right", ylim=(-1.0, 1.0))
+    s = io.BytesIO()
+    plt.savefig(s, format="png", bbox_inches='tight', metadata={'Date': None, 'Creator': None})
+    png = s.getbuffer().tobytes()
+    with open("tests/ref_kpath_right.png", "rb") as file:
+        f = file.read()
+        assert png == f
+    #with open("tests/ref_kpath_right.png", "wb") as file:
+    #    file.write(png)
 
 
 def test_kpaths_interpolate():
