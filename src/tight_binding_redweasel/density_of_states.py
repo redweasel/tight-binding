improve realize symmetry fix symmetries and units in qe interface small fixes

src/tight_binding_redweasel/density_of_states.py
@@ -115,7 +115,7 @@ def cube_cut_area_com(a0, ax, ay, az):
     com +=  v6[...,None]**2 * (np.reshape(( 1,  1, -1), shape)/2 + d * v6[...,None])
     com -=  v7[...,None]**2 * (np.reshape(( 1,  1,  1), shape)/2 + d * v7[...,None])
     com *= np.stack((sx, sy, sz), axis=-1)
-    com = np.where(area[...,None] != 0, com / area[...,None], np.reshape((0, 0, 0), shape))
+    com = com / np.where(area[...,None] != 0, area[...,None], np.inf)
     axyz = ax * ay * az
     area = np.where(np.abs(axyz) != 0, area / (2 * axyz), 0)
     return area, com

src/tight_binding_redweasel/fermi_surface.py
@@ -19,14 +19,12 @@ def fermi_surface(model, fermi_energy, k_smpl):
     return volumes, indices
 
 
-def plot_3D_fermi_surface(model, fermi_energy, N=32, elev=35, azim=20):
+def plot_3D_fermi_surface(model, fermi_energy, N=32, elev=35, azim=20, k_range=[-0.5, 0.5]):
     from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+    import matplotlib
     import matplotlib.colors as mcolors
     from skimage import measure
-    x_ = np.linspace(-.5, .5, N)
-    y_ = np.linspace(-.5, .5, N)
-    z_ = np.linspace(-.5, .5, N)
-    x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
+    x, y, z = np.meshgrid(*3*[np.linspace(*k_range, N)], indexing='ij')
     xyz = np.stack([x, y, z], axis=-1)
 
     # define light source
@@ -53,27 +51,24 @@ def plot_3D_fermi_surface(model, fermi_energy, N=32, elev=35, azim=20):
     # with mayavi (see skimage.measure.marching_cubes docstring).
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
-    mesh = Poly3DCollection(np.array(all_verts)/N - 0.5, antialiased=False) # in 3.7 there is shade=True
+    mesh = Poly3DCollection(np.array(all_verts)/N * (k_range[1] - k_range[0]) + k_range[0], antialiased=False)
     mesh.set_edgecolor(None)
     mesh.set_facecolor(all_rgb)
     ax.add_collection3d(mesh)
-    ax.set_xlim(-0.5, 0.5)
-    ax.set_ylim(-0.5, 0.5)
-    ax.set_zlim(-0.5, 0.5)
+    ax.set_xlim(*k_range)
+    ax.set_ylim(*k_range)
+    ax.set_zlim(*k_range)
     ax.set_aspect("equal")
     ax.view_init(elev=elev, azim=azim)
 
     plt.tight_layout()
     plt.show()
 
-def export_3D_fermi_surface(file, model, fermi_energy, N=32):
+def export_3D_fermi_surface(file, model, fermi_energy, N=32, k_range=[-0.5, 0.5]):
     from skimage import measure
     import matplotlib.colors as mcolors
 
-    x_ = np.linspace(-.5, .5, N)
-    y_ = np.linspace(-.5, .5, N)
-    z_ = np.linspace(-.5, .5, N)
-    x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
+    x, y, z = np.meshgrid(*3*[np.linspace(*k_range, N)], indexing='ij')
     xyz = np.stack([x, y, z], axis=-1)
 
     # use default colors from matplotlib

src/tight_binding_redweasel/kpaths.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Callable
 import numpy as np
 from . import symmetry as _sym
 import _collections_abc
@@ -174,18 +174,31 @@ class KPath(_collections_abc.Sequence):
             k_points = k_points + f"{x*2} {y*2} {z*2} 1\n"
         return k_points
 
-# given band structure data and (non hexagonal) symmetry, return an interpolator for the bandstructure.
-# This works only for data, which is arranged in a rectilinear grid after symmetrization.
-# sym needs to be a Symmetry instance from symmetry.py
-def interpolate(k_smpl, bands, sym: _sym.Symmetry = None, method="cubic"):
+
+def interpolate(k_smpl, bands, sym: _sym.Symmetry = None, method="cubic", periodic=True) -> Callable:
+    """Given band structure data and symmetry, return an 1D/2D/3D interpolator for the bandstructure.
+    This works only for data, which can be arranged in a rectilinear grid after symmetrization.
+
+    Args:
+        k_smpl (arraylike(N_k, dim(k))): k-points for interpolation
+        bands (arraylike(N_k, N_B)): (band) data for the k-points
+        sym (Symmetry, optional): symmetry for realize_symmetric_data on the data to complete the grid. Defaults to None.
+        method (str, optional): see `scipy.interpolate.RegularGridInterpolator(method=...)`. Defaults to "cubic".
+        periodic (bool, optional): if True, the data will be wrapped in a [-0.5, 0.5] unit cell and the interpolator will work for any k. Defaults to True.
+
+    Returns:
+        scipy.interpolate.RegularGridInterpolator: interpolator for the data
+    """
     import scipy.interpolate as interp
+    assert np.shape(k_smpl)[0] == np.shape(bands)[0], f"number of k_smpl and bands needs to match, but was k_smpl: ({np.shape(k_smpl)}), bands: ({np.shape(bands)})"
     dim = len(k_smpl[0])
     if sym is not None:
-        assert sym.dim() == dim, "dimensions of the symmetry and the data don't match"
-        k_smpl, bands = sym.realize_symmetric_data(k_smpl, bands)
+        assert sym.dim() == dim, f"dimensions of the symmetry and the k_smpl data don't match, symmetry: {sym.dim()}, k_smpl: {dim}"
+        k_smpl_orig = k_smpl
+        k_smpl, bands = sym.realize_symmetric_data(k_smpl, bands, unit_cell=periodic)
     n = round(len(k_smpl)**(1/dim))
     assert n**dim == len(k_smpl), "could reconstruct full square/cubic volume"
-    
+
     # sort (again) by x, y, z compatible with reshape to meshgrid
     bands = np.array(bands)
     k_smpl = np.array(k_smpl)
@@ -197,24 +210,37 @@ def interpolate(k_smpl, bands, sym: _sym.Symmetry = None, method="cubic"):
         bands = bands[reorder]
     
     used_k_smpl = k_smpl.reshape((n,)*dim + (dim,)).T
+    used_bands = bands.reshape((n,)*dim + (-1,)) # TODO is transpose needed here as well??? Probably yes!
+    if periodic:
+        # TODO make it work for k outside of the original k_smpl range by
+        # 1. extending the range of the data using periodic points
+        # 2. wrapping the function argument of the returned function using % 1.0
+        for i in range(dim):
+            vec = np.zeros(dim)
+            vec[i] = 1.0
+            vec = vec.reshape((dim,) + (1,)*dim)
+            used_k_smpl = np.concatenate([used_k_smpl.take([-1], axis=i+1, mode="wrap") - vec, used_k_smpl, used_k_smpl.take([0], axis=i+1, mode="wrap") + vec], axis=i+1)
+            used_bands = np.concatenate([used_bands.take([-1], axis=i, mode="wrap"), used_bands, used_bands.take([0], axis=i, mode="wrap")], axis=i)
+    
     if dim == 1:
-        return interp.RegularGridInterpolator(used_k_smpl,
-                                                bands.reshape((n,)*dim + (-1,)),
-                                                method=method)
+        interp_f = interp.RegularGridInterpolator(used_k_smpl, used_bands, method=method)
     if dim == 2:
-        return interp.RegularGridInterpolator((used_k_smpl[0][:,0], used_k_smpl[1][0,:]),
-                                                bands.reshape((n,)*dim + (-1,)),
+        interp_f = interp.RegularGridInterpolator((used_k_smpl[0][:,0], used_k_smpl[1][0,:]),
+                                                used_bands,
                                                 method=method)
     if dim == 3:
-        return interp.RegularGridInterpolator((used_k_smpl[0][:,0,0], used_k_smpl[1][0,:,0], used_k_smpl[2][0,0,:]),
-                                                bands.reshape((n,)*dim + (-1,)),
+        interp_f = interp.RegularGridInterpolator((used_k_smpl[0][:,0,0], used_k_smpl[1][0,:,0], used_k_smpl[2][0,0,:]),
+                                                used_bands,
                                                 method=method)
+    if periodic:
+        return lambda k: interp_f((k + 0.5) % 1.0 - 0.5)
+    return interp_f
 
 # given band structure data and symmetry, return an interpolator for the bandstructure.
 # The interpolator will return NaN if the point was not in the data.
 # So this function doesn't really interpolate.
 # This is useful for plotting the data along a path
-def interpolate_unstructured(k_smpl, bands, sym: _sym.Symmetry = None, max_error=1e-3):
+def interpolate_unstructured(k_smpl, bands, sym: _sym.Symmetry = None, max_error=1e-3) -> Callable:
     from scipy.spatial import KDTree
     dim = len(k_smpl[0])
     if sym is not None:

src/tight_binding_redweasel/qespresso_interface.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 # simple interface for QUANTUM ESPRESSO
-# assuming quantum espresso is installed on linux
+# assuming quantum espresso is installed and in the PATH
 
 ibrav_map = { "none": 0, "sc": 1, "fcc": 2, "bcc": 3, "hex": 4, "tri": 5, "monoclinic": 12 }
 
@@ -21,7 +21,16 @@ def qe_prepare(pseudo_potential_files_dict):
         os.mkdir("./pseudo")
 
     # TODO checkout https://github.com/dalcorso/pslibrary for automation of this process?
-    for file in pseudo_potential_files_dict.values():
+    for name, file in pseudo_potential_files_dict.copy().items():
+        if file is None:
+            # try to automatically choose the filename
+            # TODO these are close, but not correct...
+            # they are missing the valence tag spd
+            # a correct name would be "Cu.rel-pbesol-dn-kjpaw_psl.1.0.0.UPF"
+            #                                        ^
+            # To get those one would need to query f"http://pseudopotentials.quantum-espresso.org/legacy_tables/ps-library/{name.lower()}"
+            file = name + ".rel-pbesol-n-kjpaw_psl.1.0.0.UPF"
+            pseudo_potential_files_dict[name] = file
         if not os.path.isfile("./pseudo/" + file):
             os.chdir("./pseudo")
             os.system("wget https://pseudopotentials.quantum-espresso.org/upf_files/" + file)
@@ -55,6 +64,92 @@ def bcc(a):
 # create a hexagonal lattice with spacing a and height c
 def hexagonal(a, c):
     return np.array([[1,0,0], [-1/2,3**.5/2,0], [0,0,c/a]]).T*a
+
+# Triclinic (aP) C_i point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def triclinic(a: float, b: float, c: float, alpha: float, beta: float, gamma: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    assert np.pi > alpha and alpha > 0.0 and np.pi > beta and beta > 0.0 and np.pi > gamma and gamma > 0.0, f"all angles need to be in the range (0, π), but were α={alpha/np.pi:.4}π, β={beta/np.pi:.4}π, γ={gamma/np.pi:.4}π"
+    assert alpha + beta + gamma < np.pi*2, f"invalid combination of angles α={alpha/np.pi:.4}π, β={beta/np.pi:.4}π, γ={gamma/np.pi:.4}π (inner angle sum must by smaller than 2π, but was {(alpha + beta + gamma)/np.pi}π)"
+    bc = np.cos(beta)
+    bs = np.sin(beta)
+    gc = np.cos(gamma)
+    gs = np.sin(gamma)
+    v = (np.cos(alpha) - gc*bc) / gs
+    assert v*v < bs*bs, f"invalid combination of angles α={alpha/np.pi:.4}π, β={beta/np.pi:.4}π, γ={gamma/np.pi:.4}π (one angle is bigger than the other two combined)"
+    return np.array([[a, 0.0, 0.0],
+                [gc * b, np.sin(gamma) * b, 0.0],
+                [bc * c, v * c, (bs*bs - v*v)**.5 * c]]).T
+
+# Monoclinic (mP) C_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def monoclinic(a: float, b: float, c: float, beta: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    assert np.pi > beta and beta > 0.0, f"angle needs to be in the range (0, π), but was β={beta/np.pi:.4}π"
+    return np.array([[a, 0.0, 0.0],
+                [0.0, b, 0.0],
+                [np.cos(beta) * c, 0.0, np.sin(beta) * c]]).T
+
+# Base centered monoclinic (mS) C_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def monoclinic_base_centered(a: float, b: float, c: float, beta: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    assert np.pi > beta and beta > 0.0, f"angle needs to be in the range (0, π), but was β={beta/np.pi:.4}π"
+    return np.array([[0.5*a, -0.5*b, 0.0],
+                [0.5*a, 0.5*b, 0.0],
+                [np.cos(beta) * c, 0.0, np.sin(beta) * c]]).T
+
+# Orthorhombic (oP) D_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def orthorhombic(a: float, b: float, c: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    return np.array([[a, 0.0, 0.0],
+                [0.0, b, 0.0],
+                [0.0, 0.0, c]]).T
+
+# Base centered orthorhombic (oP) D_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def orthorhombic_base_centered(a: float, b: float, c: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    return np.array([[0.5*a, -0.5*b, 0.0],
+                [0.5*a, 0.5*b, 0.0],
+                [0.0, 0.0, c]]).T
+
+# Body centered orthorhombic (oI) D_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def orthorhombic_body_centered(a: float, b: float, c: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    return np.array([[-0.5*a, 0.5*b, 0.5*c],
+                [0.5*a, -0.5*b, 0.5*c],
+                [0.5*a, 0.5*b, -0.5*c]]).T
+
+# Face centered orthorhombic (oF) D_2h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def orthorhombic_face_centered(a: float, b: float, c: float):
+    assert a > 0.0 and b > 0.0 and c > 0.0, f"all lengths a, b, c need to be positive, but were {a:.3e}, {b:.3e}, {c:.3e}"
+    return np.array([[0.0, 0.5*b, 0.5*c],
+                [0.5*a, 0.0, 0.5*c],
+                [0.5*a, 0.5*b, 0.0]]).T
+
+# Tetragonal (tP) D_4h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def tetragonal(a: float, c: float):
+    assert a > 0.0 and c > 0.0, f"all lengths a, c need to be positive, but were {a:.3e}, {c:.3e}"
+    return np.array([[a, 0.0, 0.0],
+                [0.0, a, 0.0],
+                [0.0, 0.0, c]]).T
+
+# Body centered tetragonal (tI) D_4h point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def tetragonal_body_centered(a: float, c: float):
+    assert a > 0.0 and c > 0.0, f"all lengths a, c need to be positive, but were {a:.3e}, {c:.3e}"
+    return np.array([[-0.5*a, 0.5*a, 0.5*c],
+                [0.5*a, -0.5*a, 0.5*c],
+                [0.5*a, 0.5*a, -0.5*c]]).T
+
+# Rhombohedral (hR) D_3d point group (Schönflies notation https://en.wikipedia.org/wiki/Schoenflies_notation)
+def rhombohedral(a: float, alpha: float):
+    assert a > 0.0, "length a needs to be positive, but was {a:.3e}"
+    # basis in which the crystal stands upright
+    # -> reciprocal is also of the same type, but rotated by 180°
+    assert alpha > 0.0 and alpha < 2.0*np.pi/3, f"angle needs to be in the range (0, 2π/3), but was α={alpha/np.pi:.4}π"
+    v = 2.0/3.0 - 2.0/3.0*np.cos(alpha)
+    h = (1.0 - v)**.5 * a
+    r = v**.5 * a
+    return np.array([[r, 0.0, h],
+                [-0.5*r, 3**.5/2 * r, h],
+                [-0.5*r, -3**.5/2 * r, h]]).T
 
 class QECrystal:
     # name - the prefix for the files
@@ -251,6 +346,7 @@ class QECrystal:
         S = []
         S_trans = []
         to_eV = 27.21138625 # from Hartree = 2Ry to 1eV
+        bohr_to_angstrom = 0.52917721 # bohr_radius in Angstrom
         filename = f"./qe-data/{self.name}.save/data-file-schema.xml" if incomplete else f"./qe-data/{self.name}.xml"
         with open(filename, 'r') as file:
             from xml.dom.minidom import parse
@@ -264,10 +360,14 @@ class QECrystal:
             reciprocal = np.array([b1, b2, b3]).T
             inv_reciprocal = np.linalg.inv(reciprocal)
 
+            # This A is in bohr_radius -> convert it to Angstrom
             a1 = [float(x) for x in document.getElementsByTagName("a1")[0].firstChild.nodeValue.strip().split()]
             a2 = [float(x) for x in document.getElementsByTagName("a2")[0].firstChild.nodeValue.strip().split()]
             a3 = [float(x) for x in document.getElementsByTagName("a3")[0].firstChild.nodeValue.strip().split()]
-            A = np.array([a1, a2, a3]).T
+            A = np.array([a1, a2, a3]).T * bohr_to_angstrom
+            # TODO consider returning "reciprocal" if it is too difficult to recontruct it
+            # inv_reciprocal and A.T are collinear, but with what factor???
+            #assert np.linalg.norm(inv_reciprocal - (A.T / np.linalg.norm(A[0]))) < 1e-7, f"reciprocal lattice doesn't match real lattice... This problem comes from Quantum Espresso. The compared matrices were\n{inv_reciprocal}\nand\n{A.T / np.linalg.norm(A[0])}"
             # now find all the data in the xml file
             symmetry_list = document.getElementsByTagName("symmetry")
             for sym in symmetry_list:
@@ -290,20 +390,27 @@ class QECrystal:
             # transform k_points to crystal coordinates
             k_points = (inv_reciprocal @ np.array(k_points).T).T
             # transform symmetries to crystal coordinates
+            # HOWEVER: they seem to already be in crystal coordinates!
+            # HOWEVER: they are for the real lattice, while I only care about the symmetries for the reciprocal lattice.
             if len(S) == 0:
                 # ???
                 S = np.eye(3)[None,...]
             else:
-                S = np.einsum("nkl,ik,lj->nij", S, inv_reciprocal, reciprocal)
+                #S = np.einsum("ik,nkl,lj->nij", inv_reciprocal, S, reciprocal)
                 # TODO translations should also be transformed!
+
+                # transform S from the real space symmetries to the reciprocal space symmetries.
+                S = np.array(S).swapaxes(-1, -2)
+                S = np.linalg.inv(S)
             # symmetries should be orthogonal in this basis. TODO sometimes they are not??? Related to fractional symmetries...
             if len(S_trans) == 0:
-                assert np.linalg.norm(np.einsum("nij, nik -> njk", S, S) - np.eye(len(S[0]))) < 1e-5, "symmetries in crystal space are not orthogonal"
+                assert np.linalg.norm(np.einsum("nij,nik->njk", S, S) - np.eye(len(S[0]))) < 1e-5, "symmetries in crystal space are not orthogonal"
             else:
                 # only return the symmetries without translational part
                 S = [S for S, t in zip(S, S_trans) if np.linalg.norm(t) < 1e-10]
-                # those symmetries without translation should be orthogonal!
+                # those symmetries without translation should be orthogonal! (nope...)
                 #assert np.linalg.norm(np.einsum("nij, nik -> njk", S, S) - np.eye(len(S[0]))) < 1e-5, "symmetries in crystal space are not orthogonal"
+                print("WARNING: fractional symmetries dropped")
             fermi_energy_node = document.getElementsByTagName("fermi_energy")[0]
             fermi_energy = float(fermi_energy_node.firstChild.nodeValue.strip()) * to_eV
         return np.array(k_points), np.array(bands), S, fermi_energy, A
@@ -520,6 +627,11 @@ class QECrystal:
         if k_grid_size3 is None:
             k_grid_size3 = k_grid_size
         with open(f"{self.name}.scf.in", "w") as file:
+            # Notes:
+            # The following is just for non relativistic calculations
+            # for relativistic calculations, use the additional parameters
+            # lspinorb=.true., noncolin=.true.
+            # TODO celldm(1) is no longer allowed to be specified if CELL_PARAMETERS are used...
             file.write(f"""
 &control
     calculation='scf',

tests/test_kpath.py
@@ -52,9 +52,18 @@ def test_kpaths_interpolate():
     np.random.seed(13**5)
     k_smpl = np.array([(0, 0), (0, 0.5), (0, 1), (0.5, 0.5), (0.5, 1), (1, 1)])
     bands = np.random.random((len(k_smpl), 2))
-    interp = kpaths.interpolate(k_smpl, bands, sym=symmetry.Symmetry.square())
+    interp = kpaths.interpolate(k_smpl, bands, sym=symmetry.Symmetry.square(), periodic=False)
     for k, band in zip(k_smpl, bands):
         assert np.linalg.norm(interp(k) - band) < 1e-12, f"{interp(k)} != {band}"
         assert np.linalg.norm(interp(-k) - band) < 1e-12, f"{interp(-k)} != {band}"
 
-test_kpaths_plot_svg()
+
+def test_kpaths_interpolate():
+    np.random.seed(13**5)
+    k_smpl = np.array([(0, 0), (0, 0.5), (0.5, 0.5)])
+    bands = np.random.random((len(k_smpl), 2))
+    interp = kpaths.interpolate(k_smpl, bands, sym=symmetry.Symmetry.square(), periodic=True)
+    for k, band in zip(k_smpl, bands):
+        assert np.linalg.norm(interp(k) - band) < 1e-12, f"{interp(k)} != {band}"
+        assert np.linalg.norm(interp(-k) - band) < 1e-12, f"{interp(-k)} != {band}"
+        assert np.linalg.norm(interp(k+1) - band) < 1e-12, f"{interp(k+1)} != {band}"